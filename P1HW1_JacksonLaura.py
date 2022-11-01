453421a4bc294b4dcb1a0e01e3b10ebac8fa4831
Laura K Jackson <jacksonl2074@student.faytechcc.edu>
2022-11-01 11:48:41 -0400

    Create P1HW1_JacksonLaura.py

---
+++ P1HW1_JacksonLaura.py
@@ -0,0 +1,16 @@
+# CTI110
+# P1HW1
+# Simple Input Output
+
+#Ask the user for a number
+print ("Please enter a number: ")
+num = int( input() )
+
+print("Your number is", num)
+
+print("That number squared is", num * num)
+
+#print num + 5
+print (num + 5)
+#print num * 5
+print (num * 5)
