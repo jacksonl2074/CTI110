2dcf8aa0d4d9f3a1355149d2aa8d02e6789acc5c
Laura K Jackson <jacksonl2074@student.faytechcc.edu>
2022-11-03 10:49:18 -0400

    Create P1HW2_expenses_JacksonLaura.py

---
+++ P1HW2_expenses_JacksonLaura.py
@@ -0,0 +1,36 @@
+#CTI 110
+#P1HW2 - Expenses
+#JacksonL
+#11/3
+
+#input: three expenses, the budget
+#processing: total the expenses, compare with budget
+#output: did yougo over or u nder
+
+#our variables - type float, because of the decimal place
+gas = 0.0
+food = 0.0
+hotel = 0.0
+totalExpenses = 0.0 # gas + food + hotel
+budget = 0.0
+destination = ""
+
+print("This program calculates on displays travel expenses")
+
+print("Enter your budget: ")
+budget = float(input())
+
+destination = input("Where are you going? ")
+
+print("Ok, let's total all your expenses.")
+print("Gas: $ ")
+gas = float(input())
+
+print("Hotel: $")
+hotel = float(input())
+
+print("Food: $ ")
+food = float(input())
+
+#add up everything
+totalExpenses = gas + hotel + food
