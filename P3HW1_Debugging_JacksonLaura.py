4a52e8770b0e8791e55708a9276d207c51375e98
Laura K Jackson <116817238+jacksonl2074@users.noreply.github.com>
2022-11-22 23:03:58 -0500

    Add files via upload
    Debugged simple program that calculates grades

---
+++ P3HW1_Debugging_JacksonLaura.py
@@ -0,0 +1,36 @@
+8# CTI 110-2001
+# P3HW1_Debugging
+# JacksonLaura
+# 11.22.22
+
+
+def main():
+    # This program takes a number grade and outputs a letter grade.
+    
+    # system uses 10-point grading scale
+    
+    # TO DO: define the rest of the scores
+   
+    main()
+
+score = int(input('Enter grade: '))
+
+if score >= 90:
+    print('Your grade is: A')
+elif score >= 80 and score < 90:
+    print('Your grade is: B')
+elif score >= 70 and score < 80:
+    print('Your grade is: C')
+elif score >= 60 and score < 70:
+    print('Your grade is: D')
+else:
+    print('Your grade is: F') # TO DO: finish this
+
+
+
+
+
+
+
+# program start
+
